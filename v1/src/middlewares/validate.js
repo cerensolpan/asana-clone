f2a8b758bf48012b03f86a3a5fd143f4ff9845a4
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2021-11-02 00:44:55 +0300

    Add joi for validation

---
+++ v1/src/middlewares/validate.js
@@ -0,0 +1,16 @@
+const httpStatus = require("http-status");
+
+const validate = (schema) => (req,res,next) => {
+    const {value,error}=schema.validate(req.body);
+    if(error){
+        // error.details = [{message: ""}, {message: ""}]
+        const errorMessage = error.details?.map(detail => detail.message).join(", ") 
+        // ["","",""] => "aaa,bbb,ccc"
+        res.status(httpStatus.BAD_REQUEST).json({error: errorMessage})
+        return;
+    }
+    Object.assign(req,value);
+    return next();
+}
+
+module.exports = validate;
---
+++ v1/src/validations/Projects.js
@@ -0,0 +1,9 @@
+const Joi = require("joi");
+
+const createValidation = Joi.object({
+    name: Joi.string().required().min(5),
+});
+
+module.exports = {
+    createValidation,
+}
