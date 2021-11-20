1378b4a1ebbe29e78a72dc4781c851001b5184c0
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2021-11-20 15:28:44 +0300

    Add validation using joi

--- v1/src/api-routes/Projects.js
+++ v1/src/api-routes/Projects.js
@@ -1,11 +1,13 @@
-//validations
 //validate middleware
-
+const validate =require("../middlewares/validate")
+//validations
+const schemas = require("../validations/Projects")
 const express = require("express");
 const {create,index} = require("../controllers/Projects")
 const router = express.Router();
 
 router.get("/",index);
 router.post("/",create);
+router.route("/").post(validate(schemas.createValidation),create);
 
 module.exports = router
