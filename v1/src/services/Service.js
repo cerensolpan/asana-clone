2fc059aff3e11fb9b257613f04a29ee40b73da23
cerensolpan <cerensolpan@gmail.com>
2022-01-06 00:15:20 +0300

    Delete Service.js

--- v1/src/services/Service.js
+++
@@ -1,9 +0,0 @@
-const BaseService = require ("./BaseService")
-class Service extends BaseService {
-    getService2(){
-        console.log('"Service2" :>> ', "Service2");
-    }
-}
-
-
-module.exports = Service;
